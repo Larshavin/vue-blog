620b9b6f4aa2b128fa4bab3076d9387f9c2d623d
Larshavin <gkdlgkdl2040@naver.com>
2024-05-11 23:12:17 +0900

    dist 추적하지 않음

--- init_submodule.sh
+++ init_submodule.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+mkdir dist
+
+cd dist
+git init
+git remote add origin https://github.com/Larshavin/Larshavin.github.io.git
+git checkout main
+cd ..
+
+git submodule add https://github.com/Larshavin/Larshavin.github.io.git dist
+
 git submodule init
-rm -rf /dist/*
 git submodule update
