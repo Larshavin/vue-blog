0645ccfe0ae55aec597a86702ad06a9df89ee4b5
Larshavin <gkdlgkdl2040@naver.com>
2024-05-11 23:18:28 +0900

    rebuilding site Sat May 11 11:18:28 PM KST 2024

--- init_submodule.sh
+++ init_submodule.sh
@@ -4,10 +4,11 @@ mkdir dist
 cd dist
 git init
 git remote add origin https://github.com/Larshavin/Larshavin.github.io.git
-git checkout main
+git branch --set-upstream-to=origin/main main
+git pull origin main
 cd ..
 
-git submodule add https://github.com/Larshavin/Larshavin.github.io.git dist
-
-git submodule init
-git submodule update
+# git submodule add https://github.com/Larshavin/Larshavin.github.io.git dist
+# git submodule init
+# git submodule update
+# git submodule status
